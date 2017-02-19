5080e60b44b850b1672fb3e26368c6cbdd00e496
Joe Ardent <code@ardent.nebcorp.com>
2017-02-19 14:59:17 -0800

    Does not work; can't move self.head out of borowed content
       Compiling lists v0.1.0 (file:///home/ardent/rust-stuff/lists)
    error[E0507]: cannot move out of borrowed content
      --> src/first.rs:23:19
       |
    23 |             next: self.head,
       |                   ^^^^ cannot move out of borrowed content

---
+++ src/first.rs
@@ -0,0 +1,26 @@
+pub struct List {
+    head: Link,
+}
+
+struct Node {
+    elem: i32,
+    next: Link,
+}
+
+enum Link {
+    Empty,
+    More(Box<Node>),
+}
+
+impl List {
+    pub fn new() -> Self {
+        List { head: Link::Empty }
+    }
+
+    pub fn push(&mut self, elem: i32) {
+        let new_node = Node {
+            elem: elem,
+            next: self.head,
+        };
+    }
+}
