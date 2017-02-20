a95ea97633a4b727ecc0406ac283b74f766e6137
Joe Ardent <code@ardent.nebcorp.com>
2017-02-19 20:50:19 -0800

    Adds peek() to List.

--- src/second.rs
+++ src/second.rs
@@ -30,6 +30,14 @@ impl<T> List<T> {
             node.elem // map() returns an option already
         })
     }
+
+    pub fn peek(&self) -> Option<&T> {
+        self.head.as_ref().map(|node| &node.elem)
+    }
+
+    pub fn peek_mut(&mut self) -> Option<&mut T> {
+        self.head.as_mut().map(|node| &mut node.elem)
+    }
 }
 
 impl<T> Drop for List<T> {
@@ -80,5 +88,23 @@ mod test {
         // exhausted removal
         assert_eq!(list.pop(), None);
         assert_eq!(list.pop(), None);
+    }
+
+    #[test]
+    fn peekaboo() {
+        let mut list = List::new();
+
+        list.push(1);
+        list.push(2);
+
+        assert_eq!(list.peek(), Some(&2));
+
+        {
+            let head: &mut u32 = list.peek_mut().unwrap();
+            *head = 3;
+        }
+
+        assert_eq!(list.peek(), Some(&3));
+
     }
 }
