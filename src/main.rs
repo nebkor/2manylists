6a22bb292131c5f420d4759140b04ba4fc99a0d6
Joe Ardent <code@ardent.nebcorp.com>
2017-02-23 15:22:22 -0800

    Adds main.rs ad-hoc testing.
    This program demonstrates that if you're taking care of ownership
    and move semantics, you can create an immutable iterator from a
    lists::second::List, unlike the test case in the test sub-module.

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+extern crate lists;
+
+use std::collections::VecDeque;
+use lists::second::List;
+
+
+fn main() {
+    {
+        // Create a Vector of values.
+        let input = vec![1, 2, 3];
+        let iterator = input.iter();
+        let mapped = iterator.map(|&x| {
+            return x * 2;
+        });
+        // Gather the result in a RingBuf.
+        let output = mapped.collect::<VecDeque<_>>();
+        println!("{:?}", output);
+    }
+
+    {
+        let mut list = List::new();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        let iter = list.iter();
+        let mapped = iter.map(|&x| {
+            return Some(x);
+        });
+
+        let output = mapped.collect::<Vec<_>>();
+        println!("vector from list: {:?}", output);
+    }
+}
+// Outputs [2, 4, 6]
