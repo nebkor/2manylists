509c5f0f05118c0b9bc70c447cd173e0dfd478b6
Joe Ardent <code@ardent.nebcorp.com>
2017-03-09 16:39:00 -0500

    Finishes working IterMut.

--- src/second.rs
+++ src/second.rs
@@ -4,7 +4,7 @@ pub struct Iter<'a, T: 'a> {
     next: Option<&'a Node<T>>,
 }
 
-pub struct IterMute<'a, T: 'a> {
+pub struct IterMut<'a, T: 'a> {
     next: Option<&'a mut Node<T>>,
 }
 
@@ -56,6 +56,10 @@ impl<T> List<T> {
     pub fn iter(&self) -> Iter<T> {
         Iter { next: self.head.as_ref().map(|node| &**node) }
     }
+
+    pub fn iter_mut(&mut self) -> IterMut<T> {
+        IterMut { next: self.head.as_mut().map(|node| &mut **node) }
+    }
 }
 
 impl<T> Drop for List<T> {
@@ -86,12 +90,24 @@ impl<'a, T> Iterator for Iter<'a, T> {
     type Item = &'a T;
 
     fn next(&mut self) -> Option<Self::Item> {
-        self.next.map(|node| {
+        self.next.take().map(|node| {
             self.next = node.next.as_ref().map(|node| &**node);
             &node.elem
         })
     }
 }
+
+impl<'a, T> Iterator for IterMut<'a, T> {
+    type Item = &'a mut T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.take().map(|node| {
+            self.next = node.next.as_mut().map(|node| &mut **node);
+            &mut node.elem
+        })
+    }
+}
+
 
 #[cfg(test)]
 mod test {
@@ -169,5 +185,19 @@ mod test {
         assert_eq!(iter.next(), Some(&5));
         assert_eq!(iter.next(), Some(&1));
         assert_eq!(iter.next(), None);
+    }
+
+    #[test]
+    fn iter_mut() {
+        let mut list = List::new();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        let mut iter = list.iter_mut();
+        assert_eq!(iter.next(), Some(&mut 3));
+        assert_eq!(iter.next(), Some(&mut 2));
+        assert_eq!(iter.next(), Some(&mut 1));
+
     }
 }
