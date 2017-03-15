bd6e6695dd7059dace95530d3ed94f9d6343f7c2
Joe Ardent <code@ardent.nebcorp.com>
2017-03-15 15:18:25 -0400

    Implements Drop trait for List, but doesn't work.
    Gives the following error:
    
    error[E0308]: mismatched types
      --> src/third.rs:63:24
       |
    63 |                 head = node.next.take();
       |                        ^^^^^^^^^^^^^^^^ expected struct `third::Node`, found enum `std::option::Option`
       |
       = note: expected type `third::Node<T>`
       = note:    found type `std::option::Option<std::rc::Rc<third::Node<T>>>`
    
    error: aborting due to previous error

--- src/third.rs
+++ src/third.rs
@@ -55,6 +55,18 @@ impl<'a, T> Iterator for Iter<'a, T> {
     }
 }
 
+impl<T> Drop for List<T> {
+    fn drop(&mut self) {
+        let mut head = self.head.take();
+        while let Some(node) = head {
+            if let Ok(mut head) = Rc::try_unwrap(node) {
+                head = node.next.take();
+            } else {
+                break;
+            }
+        }
+    }
+}
 
 #[cfg(test)]
 mod test {
