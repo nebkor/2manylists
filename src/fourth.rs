a468e9683c499f493bc97fa88e39ac7ac50f3583
Joe Ardent <code@ardent.nebcorp.com>
2017-03-16 17:28:37 -0400

    Uses RefCell incorrectly.
    Gives the following error:
    
    cargo build
       Compiling lists v0.1.0 (file:///home/ardent/rust-stuff/lists)
    error: no field `prev` on type `std::rc::Rc<std::cell::RefCell<fourth::Node<T>>>`
      --> fourth.rs:41:26
       |
    41 |                 old_head.prev = Some(new_head.clone()); // +1 new_head
       |                          ^^^^ unknown field
    
    error: no field `next` on type `std::rc::Rc<std::cell::RefCell<fourth::Node<T>>>`
      --> fourth.rs:42:26
       |
    42 |                 new_head.next = Some(old_head); // +1 old_head
       |                          ^^^^ unknown field
    
    error: aborting due to 2 previous errors

---
+++ src/fourth.rs
@@ -0,0 +1,52 @@
+use std::rc::Rc;
+use std::cell::RefCell;
+
+pub struct List<T> {
+    head: Link<T>,
+    tail: Link<T>,
+}
+
+type Link<T> = Option<Rc<RefCell<Node<T>>>>;
+
+struct Node<T> {
+    elem: T,
+    next: Link<T>,
+    prev: Link<T>,
+}
+
+impl<T> Node<T> {
+    fn new(elem: T) -> Rc<RefCell<Self>> {
+        Rc::new(RefCell::new(Node {
+            elem: elem,
+            prev: None,
+            next: None,
+        }))
+    }
+}
+
+impl<T> List<T> {
+    pub fn new() -> Self {
+        List {
+            head: None,
+            tail: None,
+        }
+    }
+
+    pub fn push_front(&mut self, elem: T) {
+        // new node needs +2 links, everything else should be +0
+        let new_head = Node::new(elem);
+        match self.head.take() {
+            Some(old_head) => {
+                // non-empty list, need to connect the old head
+                old_head.prev = Some(new_head.clone()); // +1 new_head
+                new_head.next = Some(old_head); // +1 old_head
+                self.head = Some(new_head); // +1 new_head, -1 old_head
+            }
+            None => {
+                // empty list, need to set the tail
+                self.tail = Some(new_head.clone()); // +1 new_head
+                self.head = Some(new_head);
+            }
+        }
+    }
+}
